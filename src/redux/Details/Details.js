abcf3439905e096ccd9c53775ba6178c007706a2
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-19 08:27:31 +0300

    Modify coins detail page

--- src/redux/Details/Details.js
+++ src/redux/Details/Details.js
@@ -1,21 +1,20 @@
 /* eslint-disable */
 
-const BASE_URL = 'https://api.coinstats.app/public/v1/coins/';
+const BASE_URL = 'https://api.coingecko.com/api/v3/coins/';
 const GET_COIN_DATA = 'GET_COIN_DATA';
 
 export const fetchCoinsDetailsApi = async(coin) => {
   const response = await fetch(`${BASE_URL}${coin}`);
   const data = await response.json();
-  const coinData = data.coin
-  const details =[coinData].map(coin => ({
+  const details =[data].map(coin => ({
     id: coin.id,
     name: coin.name,
-    image: coin.icon,
-    price: coin.price,
-    rank: coin.rank,
-    priceChange1h: coin.priceChange1h,
-    priceChange1d: coin.priceChange1d,
-    priceChange1w: coin.priceChange1w
+    image: coin.image.large,
+    price: coin.market_data.current_price.usd,
+    rank: coin.market_cap_rank,
+    priceChange1h: coin.market_data.price_change_percentage_1h_in_currency.usd,
+    priceChange1d: coin.market_data.price_change_24h_in_currency.usd,
+    priceChange1w: coin.market_data.price_change_percentage_7d_in_currency.usd
    }))
    return details;
 } 
